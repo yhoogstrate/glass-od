e54ad082abd8c83e75b3c20e0e7917a987e16325
yhoogstrate <y.hoogstrate@gmail.com>
2023-07-20 14:30:52 +0200

    excl

--- scripts/analysis_unsupervised_qc.R
+++ scripts/analysis_unsupervised_qc.R
@@ -5,10 +5,6 @@
 
 
 library(ggplot2)
-library(minfi)
-
-library(IlluminaHumanMethylationEPICmanifest)
-
 
 
 if(!exists('youri_gg_theme')) {
@@ -16,243 +12,146 @@ if(!exists('youri_gg_theme')) {
 }
 
 
-
 if(!exists('glass_od.metadata.idats')) {
-  source('scripts/load_metadata.R')
+  source('scripts/load_GLASS-OD_metadata.R')
+}
+
+if(!exists('glass_nl.metadata.idats')) {
+  source('scripts/load_GLASS-NL_metadata.R')
+}
+
+if(!exists('gsam.metadata.idats')) {
+  source('scripts/load_G-SAM_metadata.R')
 }
 
 
-
-
-# replicates better than original? ----
-
-#' messy code and not working anymore because samples already
-#' excluded before generating the m-values 
-#' it was just to see if the replicates were indeed better
-#' only for 0003-R3 is was unclear from detP & PC1 stats
-#' but CNV profile was clear
-
-## load all samples, qc and corr w/ qc stats ----
-
-mvalue <- readRDS("cache/mvalues.Rds") |> 
-  tibble::column_to_rownames("probeID")
+source('scripts/load_functions.R')
 
 
 
-
-sum(array_samples$sentrix_id %in% colnames(mvalue)==F) # should be 0
-
-
-replicated <- glass_od.metadata.idats |>
-  dplyr::filter(duplicated(resection_id)) |>
-  dplyr::pull(resection_id)
+# load all samples & data ----
 
 
-glass_od.metadata.idats |> 
-  dplyr::filter(resection_id %in% replicated) |> 
-  dplyr::select(resection_id, resection_isolation_id, 
-              reason_excluded_patient,
-              reason_excluded_resection,
-              reason_excluded_resection_isolation,
-              reason_excluded_array_sample
-              ) |> 
-  dplyr::arrange(resection_id, resection_isolation_id) |> 
-  View()
+metadata <- rbind(
+  glass_od.metadata.idats |> 
+    dplyr::select(sentrix_id, percentage.detP.signi, mnp_QC_predicted_sample_type, qc.pca.comp1, qc.pca.outlier) |> 
+    dplyr::mutate(dataset = "GLASS-OD") # & CATNON sample -> codels & oligosarcoma's
+  ,
+  glass_nl.metadata.idats |> 
+    dplyr::select(sentrix_id, percentage.detP.signi, mnp_QC_predicted_sample_type) |> 
+    dplyr::mutate(qc.pca.comp1 = NA, qc.pca.outlier = NA) |> 
+    dplyr::mutate(dataset = "GLASS-NL") # astro's
+  ,
+  gsam.metadata.idats |> 
+    dplyr::select(sentrix_id, percentage.detP.signi, mnp_QC_predicted_sample_type) |> 
+    dplyr::mutate(qc.pca.comp1 = NA, qc.pca.outlier = NA) |> 
+    dplyr::mutate(dataset = "G-SAM")
+  ) |> 
+  assertr::verify(!is.na(sentrix_id)) |> 
+  (function(.) {
+    print(dim(.))
+    assertthat::assert_that(nrow(.) == (222 + 235 + 75))
+    return(.)
+  })()
 
 
-array_samples <- glass_od.metadata.idats |> 
-  dplyr::filter(is.na(reason_excluded_patient)) |> 
-  dplyr::filter(is.na(reason_excluded_resection))
-  #dplyr::filter(is.na(reason_excluded_resection_isolation)) 
-  #dplyr::filter(is.na(reason_excluded_array_sample))
+data <- readRDS("cache/mvalues.all_samples.Rds") |> 
+  dplyr::select(metadata$sentrix_id) |> 
+  (function(.) {
+    print(dim(.))
+    assertthat::assert_that(ncol(.) == (222 + 235 + 75))
+    return(.)
+  })()
 
-sum(duplicated(array_samples$resection_id))
 
-targets <- array_samples |> 
-  dplyr::rename(Sample_Name = resection_isolation_id) |>
-  dplyr::mutate(Array = gsub("^.+_","",sentrix_id)) |> 
-  dplyr::rename(Slide = methylation_array_chip_id) |> 
-  dplyr::mutate(Basename = gsub("_Grn.idat$","", channel_green)) |> 
-  dplyr::select(Sample_Name, sentrix_id, Array,Slide, Basename)
+
+
 
 
 ## pca ----
 
 
-pca.raw <- mvalue |> 
+pca.raw <- data |> 
   (function(.) dplyr::mutate(., mad =  apply( ., 1, stats::mad)) )() |>  # this synthax, oh my
-  dplyr::filter(mad > 1.0) |> 
-  dplyr::mutate(mad = NULL)  |>  # remove the sd to obtain original vst matrix
+  dplyr::slice_head(n=200000) |> 
+  dplyr::mutate(mad = NULL) |>  # remove the sd to obtain original vst matrix
   t() |> 
   prcomp()
 
-#saveRDS(pca.raw, "cache/analysis_supervised_qc_pca.Rds")
-#pca.raw <- readRDS("cache/analysis_supervised_qc_pca.Rds")
 
-
-pca2 <- pca.raw |> 
+tmp <- pca.raw |> 
   purrr::pluck('x')  |>   # take coordinates
   as.data.frame(stringsAsFactor=F) |>  # transform back from matrix to data.frame 
-  dplyr::select(PC1, PC2, PC3, PC4, PC5, PC6, PC7, PC8, PC9, PC10)
+  dplyr::select(paste0('PC',1:20)) |> 
+  tibble::rownames_to_column('sentrix_id')
+
+metadata <- metadata |> 
+  dplyr::left_join(tmp, by=c('sentrix_id'='sentrix_id'),suffix=c('',''))
+
+rm(tmp)
+
+
+## find the component w/ strongest cor w/ detP ----
+
+
+metadata <- metadata |> 
+  dplyr::mutate(qc.pca.detP.outlier = ifelse(PC1 >= 600 | percentage.detP.signi >= 2.5,T,F))
+
+
+cor(metadata$percentage.detP.signi, metadata$PC1, method="spearman")
+cor(metadata$percentage.detP.signi, metadata$PC2, method="spearman")
+cor(metadata$percentage.detP.signi, metadata$PC3, method="spearman")
+cor(metadata$percentage.detP.signi, metadata$PC4, method="spearman")
+
+
+ggplot(metadata, aes(x=PC1, y=percentage.detP.signi, col=qc.pca.detP.outlier)) +
+  geom_point() +
+  coord_cartesian(ylim = c(0, 25)) +
+  geom_hline(yintercept=2.5, col="red", lty=2, lwd=0.5) +
+  geom_vline(xintercept=600, col="red", lty=2, lwd=0.5) +
+  theme_bw()
+
+
+ggplot(metadata, aes(x=PC3, y=percentage.detP.signi, col=qc.pca.detP.outlier)) +
+  geom_point() +
+  coord_cartesian(ylim = c(0, 25)) +
+  geom_hline(yintercept=2.5, col="red", lty=2, lwd=0.5) +
+  geom_vline(xintercept=600, col="red", lty=2, lwd=0.5) +
+  theme_bw()
+
+
+ggplot(metadata, aes(x=PC1, y=PC3, col=qc.pca.detP.outlier)) +
+  geom_point() +
+  theme_bw()
 
 
 
-
-# add first 10 to qc
-## qc plots detP x covars ----
-# 
-# plt <- glass_od.metadata.idats |> 
-#   tibble::column_to_rownames('sentrix_id') |> 
-#   dplyr::select(contains("detP") | starts_with("qc_"))
-
-cplt <- glass_od.metadata.idats  |>
-  dplyr::mutate(`qc_BISULFITE_CONVERSION_I_Beta_I-3_Beta_larger_0.12_0.18`=NULL) |> # praktisch allemaal NA
-  dplyr::mutate(`qc_BISULFITE_CONVERSION_I_Beta_I-6_Beta_larger_0.2_0.3`=NULL) |> # praktisch allemaal NA
-  tibble::column_to_rownames('sentrix_id') |> 
-  dplyr::select(contains("detP") | starts_with("qc_")) |> 
-  dplyr::filter(!is.na(`qc_BISULFITE_CONVERSION_I_Beta_I-1_Beta_larger_0.1_0.15`)) |> 
-  dplyr::filter(!is.na(`qc_BISULFITE_CONVERSION_I_Beta_I-5_Beta_larger_0.2_0.3`)) |> 
-  dplyr::filter(!is.na(`qc_BISULFITE_CONVERSION_II_Beta_II-1_Beta_larger_0.2_0.3`)) |> 
-  dplyr::rename_with( ~ gsub("_larger.+$","", .x)) |> 
-  dplyr::rename_with( ~ gsub("_smaller.+$","", .x)) |> 
-  tibble::rownames_to_column('sentrix_id') |> 
-  dplyr::left_join(
-    pca2 |> tibble::rownames_to_column('sentrix_id'),
-    by=c('sentrix_id'='sentrix_id')
-  ) |> 
-  tibble::column_to_rownames('sentrix_id') |> 
-  dplyr::filter(!is.na(PC1)) |> 
-  #dplyr::mutate(PC1 = -1 * PC1) |> 
-  dplyr::mutate(percentage.detP.signi = -1 * percentage.detP.signi)
+ggplot(metadata, aes(x=qc.pca.comp1, y=percentage.detP.signi, col=qc.pca.detP.outlier)) +
+  geom_point() +
+  coord_cartesian(ylim = c(0, 25))
 
 
-corrplot::corrplot(abs(cor(cplt, method="spearman")),tl.cex=0.4, order="hclust")
+ggplot(metadata, aes(x=qc.pca.comp1, y=PC1, col=qc.pca.detP.outlier)) +
+  geom_point() 
 
 
-ggplot(cplt, aes(x=percentage.detP.signi, y=PC1)) +
+
+ggplot(metadata, aes(x=PC3, y=percentage.detP.signi, col=qc.pca.outlier)) +
+  geom_point() +
+  coord_cartesian(ylim = c(0, 25))
+
+ggplot(metadata, aes(x=PC1,y=PC3, col=qc.pca.outlier)) +
   geom_point()
 
 
 
-plt <- cplt |> 
-  tibble::rownames_to_column('sentrix_id') |> 
-  dplyr::left_join(
-    glass_od.metadata.idats |>  dplyr::select(sentrix_id, resection_id, resection_isolation_id),
-    by=c('sentrix_id'='sentrix_id')
-  ) |> 
-  dplyr::mutate(percentage.detP.signi = -1 * percentage.detP.signi) |> 
-  dplyr::mutate(
-    col = dplyr::case_when(
-      abs(percentage.detP.signi) > 5 ~ "detP too high (>5%)",
-      resection_id %in% c("0006-R1", "0019-R1") ~ "obfuscating results when included in n=20 test set",
-      T ~ "-"
-    )
-  ) |> 
-  dplyr::mutate(resection = gsub("^.+\\-","", resection_id))
 
-plt <- plt |> 
-       dplyr::mutate(y = PC1 + (runif(nrow(plt)) * 10))
-
-ggplot(plt 
-         #dplyr::mutate(col = resection_id %in% replicated)
-       , aes(x=PC1,
-             y=percentage.detP.signi
-             , 
-                label=resection_isolation_id, color = col,
-                group=resection_id)) +
-  geom_point() +
-  #geom_line() +
-  ggrepel::geom_text_repel( col="black") +
-  youri_gg_theme
+out <- metadata |> 
+  dplyr::select(sentrix_id, PC1, qc.pca.detP.outlier) |> 
+  dplyr::rename(qc.pca.comp1 = PC1)
 
 
-plt |> 
-  dplyr::filter(resection_id == "0003-R3") |> 
-  dplyr::pull(sentrix_id)
-
-
-
-# QC-check all of the samples ----
-
-
-mvalue <- readRDS("cache/mvalues.Rds") |> 
-  tibble::column_to_rownames("probeID") |> 
-  (function(.) {
-    assertthat::assert_that(ncol(.) == 199)
-    return(.)
-  })()
-
-
-# pca on samples of all quality
-pca.raw <- mvalue |> 
-  (function(.) dplyr::mutate(., mad =  apply( ., 1, stats::mad)) )() |>  # this synthax, oh my
-  dplyr::filter(mad > 1.0) |> 
-  dplyr::mutate(mad = NULL)  |>  # remove the sd to obtain original vst matrix
-  t() |> 
-  prcomp()
-
-
-
-plt <- pca.raw |> 
-  purrr::pluck('x')  |>   # take coordinates
-  as.data.frame(stringsAsFactor=F) |>  # transform back from matrix to data.frame 
-  dplyr::select(PC1, PC2, PC3, PC4, PC5, PC6, PC7, PC8, PC9, PC10) |> 
-  tibble::rownames_to_column('sentrix_id') |> 
-  dplyr::left_join(
-    glass_od.metadata.idats |>  dplyr::select(sentrix_id, resection_id, resection_isolation_id, patient_id),
-    by=c('sentrix_id'='sentrix_id')
-  ) |> 
-  dplyr::left_join(
-    
-    glass_od.metadata.idats  |>
-      dplyr::mutate(`qc_BISULFITE_CONVERSION_I_Beta_I-3_Beta_larger_0.12_0.18`=NULL) |> # praktisch allemaal NA
-      dplyr::mutate(`qc_BISULFITE_CONVERSION_I_Beta_I-6_Beta_larger_0.2_0.3`=NULL) |> # praktisch allemaal NA
-      tibble::column_to_rownames('sentrix_id') |> 
-      dplyr::select(contains("detP") | starts_with("qc_")) |> 
-      dplyr::filter(!is.na(`qc_BISULFITE_CONVERSION_I_Beta_I-1_Beta_larger_0.1_0.15`)) |> 
-      dplyr::filter(!is.na(`qc_BISULFITE_CONVERSION_I_Beta_I-5_Beta_larger_0.2_0.3`)) |> 
-      dplyr::filter(!is.na(`qc_BISULFITE_CONVERSION_II_Beta_II-1_Beta_larger_0.2_0.3`)) |> 
-      dplyr::rename_with( ~ gsub("_larger.+$","", .x)) |> 
-      dplyr::rename_with( ~ gsub("_smaller.+$","", .x)) |> 
-      tibble::rownames_to_column('sentrix_id'),
-    
-    by=c('sentrix_id'='sentrix_id'), suffix=c('','')
-  ) |> 
-  dplyr::mutate(
-    col = dplyr::case_when(
-      abs(percentage.detP.signi) > 5 ~ "detP too high (>5%)",
-      resection_id %in% c("0006-R1", "0019-R1") ~ "obfuscating results when included in n=20 test set",
-      T ~ "-"
-    )
-  )
-
-
-
-
-PC1.cutoff <- 425
-ggplot(plt, aes(x = PC1, y = percentage.detP.signi, label = resection_isolation_id, color = col, group = patient_id)) +
-  geom_vline(xintercept = PC1.cutoff, col = "blue", alpha = 0.5, lty = 2) +
-  # geom_line(col="black") +
-  geom_point() +
-  ggrepel::geom_text_repel(data = plt |> dplyr::filter(PC1 <= PC1.cutoff), col = "black") +
-  # ggrepel::geom_text_repel(data = plt |> dplyr::filter(PC1 > PC1.cutoff), col="black") +
-  youri_gg_theme
-
-
-
-qc.outliers <- plt |>
-  dplyr::filter(PC1 > PC1.cutoff) |>
-  assertr::verify(c("0019-R2", "0020-R1", "0042-R1", "0019-R1", "0062-R3") %in% resection_isolation_id) |>
-  assertr::verify(c("0031-R3", "0060-R1", "0054-R3", "0099-R2") %in% resection_isolation_id == F)
-
-
-out <- plt |>
-  dplyr::select(sentrix_id, PC1) |> 
-  dplyr::rename(qc.pca.comp1 = PC1) |> 
-  dplyr::mutate(qc.pca.outlier = sentrix_id %in% qc.outliers$sentrix_id)
-
-
-saveRDS(out, file="cache/unsupervised_qc_qc.outliers.Rds")
+saveRDS(out, file="cache/unsupervised_qc_outliers_all.Rds")
 
 
 
