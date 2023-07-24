1d0bce7335056ece3168191b09175f3c8ac05be5
yhoogstrate <y.hoogstrate@gmail.com>
2023-07-24 15:09:15 +0200

    update

--- scripts/analysis_O_IDH_LG__O_IDH_HG.R
+++ scripts/analysis_O_IDH_LG__O_IDH_HG.R
@@ -1,32 +1,104 @@
 #!/usr/bin/env R
 
-# make linear model to predict A_IDH ~ A_IDH_HG
+# load data ----
 
-if(!exists('glass_nl.data.mvalues')) {
-  source('scripts/load_mvalues.R')
+
+library(ggplot2)
+
+
+## m-values ----
+
+
+if(!exists('data.mvalues.hq_samples')) {
+  source('scripts/load_mvalues_hq_samples.R')
+}
+
+
+## metadata ----
+
+
+if(!exists('glass_od.metadata.idats')) {
+  source('scripts/load_GLASS-OD_metadata.R')
+}
+
+if(!exists('glass_nl.metadata.idats')) {
+  source('scripts/load_GLASS-NL_metadata.R')
+}
+
+if(!exists('gsam.metadata.idats')) {
+  source('scripts/load_G-SAM_metadata.R')
 }
 
 
 
-metadata.all <- glass_nl.metadata.resections |> 
-  dplyr::filter(!is.na(IDH_HG_IDH_ratio)) |> 
-  dplyr::mutate(i = 1:n()) |> 
-  dplyr::mutate(m = i %% 10)
+data.good.probes <- data.mvalues.mask.hq_samples |>
+  is.na() |>
+  rowSums() |>
+  as.data.frame() |>
+  dplyr::rename(n_na = 1) |> 
+  dplyr::filter(n_na == 0) |> 
+  tibble::rownames_to_column('probe_id')
 
 
-data.all <- glass_nl.data.mvalues |>
-  dplyr::select(metadata.all$methylation.sid) |>
+
+
+metadata.glass_nl <- glass_nl.metadata.idats |> 
+  dplyr::filter(!qc.pca.detP.outlier) |> 
+  assertr::verify(!is.na(A_IDH_HG__A_IDH_LG_lr)) |> 
+  dplyr::mutate(i = 1:dplyr::n()) |> 
+  dplyr::mutate(slice = i %% 10, i = NULL)
+
+
+data.glass_nl <- data.mvalues.hq_samples |>
+  tibble::rownames_to_column('probe_id') |> 
+  dplyr::filter(probe_id %in% data.good.probes$probe_id) |> 
+  tibble::column_to_rownames('probe_id') |> 
+  dplyr::select(metadata.glass_nl$sentrix_id) |> 
   (function(.) dplyr::mutate(., mad =  apply( ., 1, stats::mad)) )() |> # this synthax, oh my
   dplyr::arrange(mad) |> 
   dplyr::mutate(mad = NULL)
 
-data.all.pca.obj <- data.all |> 
+
+
+
+metadata.glass_od <- glass_od.metadata.idats |> 
+  filter_GLASS_OD_idats(163)
+
+
+data.glass_od <- data.mvalues.hq_samples |>
+  tibble::rownames_to_column('probe_id') |> 
+  dplyr::filter(probe_id %in% data.good.probes$probe_id) |> 
+  tibble::column_to_rownames('probe_id') |> 
+  dplyr::select(metadata.glass_od$sentrix_id) |> 
+  (function(.) dplyr::mutate(., mad =  apply( ., 1, stats::mad)) )() |> # this synthax, oh my
+  dplyr::arrange(mad) |> 
+  dplyr::mutate(mad = NULL)
+
+
+
+## add PCA to metadata ----
+
+
+
+
+data.glass_nl.pca.obj <- data.glass_nl |> 
   t() |> 
   prcomp()
-data.all.pca <- data.all.pca.obj |> 
+data.glass_nl.pca <- data.glass_nl.pca.obj |> 
   purrr::pluck('x') |> 
   t() |> 
   as.data.frame(stringsAsFactors=F)
+
+
+
+
+
+# make linear model to predict A_IDH ~ A_IDH_HG
+
+
+
+
+
 
 
 
@@ -35,97 +107,102 @@ data.all.pca <- data.all.pca.obj |>
 
 
 
-# purely on m-values ----
+# model 1: purely on m-values ----
 #' PCA based works a little better and considerably faster
 
-
+## train parameters ----
 
 set.seed(123456)
-cv_model_probe_based <- glmnet::cv.glmnet(t(data.all |>  dplyr::select(metadata.all$methylation.sid)),
-                                          metadata.all$IDH_HG_IDH_ratio, alpha = 1, relax=F)
+cv_model_probe_based <- glmnet::cv.glmnet(t(data.glass_nl |>  dplyr::select(metadata.glass_nl$sentrix_id)),
+                                          metadata.glass_nl$A_IDH_HG__A_IDH_LG_lr, alpha = 1, relax=F)
 plot(cv_model_probe_based) 
 
-## simple cv ----
+
+
+## test predicted vs real ----
 
 
 out.probe_based <- data.frame()
 for(cv in 0:9) {
   
-  metadata.train <- metadata.all |> dplyr::filter(m != cv)
-  metadata.test <- metadata.all |> dplyr::filter(m == cv)
+  metadata.train <- metadata.glass_nl |> dplyr::filter(slice != cv)
+  metadata.test <- metadata.glass_nl |> dplyr::filter(slice == cv)
   
   set.seed(123456)
   lm.probe_based <- glmnet::glmnet(
-    data.all |>
-      dplyr::select(metadata.train$methylation.sid) |> 
+    data.glass_nl |>
+      dplyr::select(metadata.train$sentrix_id) |> 
       t(),
-    metadata.train$IDH_HG_IDH_ratio,
+    metadata.train$A_IDH_HG__A_IDH_LG_lr,
     alpha = 1, # 1 = lasso; 0 = ridge
     lambda = cv_model_probe_based$lambda.min)
   
   
-  metadata.test$LGC.lasso.probe_based <- predict(lm.probe_based, data.all |>
-                                                   dplyr::select(metadata.test$methylation.sid) |> 
+  metadata.test$LGC.lasso.probe_based <- predict(lm.probe_based, data.glass_nl |>
+                                                   dplyr::select(metadata.test$sentrix_id) |> 
                                                    t())|> 
     as.numeric()
   
   out.probe_based <- rbind(
     metadata.test |>  
-      dplyr::select(methylation.sid, LGC.lasso.probe_based, IDH_HG_IDH_ratio, methylation.sub.diagnosis)
+      dplyr::select(sentrix_id, LGC.lasso.probe_based, A_IDH_HG__A_IDH_LG_lr, mnp_predictBrain_v12.8_cal_class)
     , out.probe_based)
 }
 rm(cv)
 
-sqrt(sum((out.probe_based$LGC.lasso.probe_based - out.probe_based$IDH_HG_IDH_ratio)^2) / length(out.probe_based$IDH_HG_IDH_ratio))
+sqrt(sum((out.probe_based$LGC.lasso.probe_based - out.probe_based$A_IDH_HG__A_IDH_LG_lr)^2) / length(out.probe_based$A_IDH_HG__A_IDH_LG_lr))
 
 
 out.probe_based <- out.probe_based |> 
-  dplyr::mutate(methylation.sub.diagnosis = ifelse(methylation.sub.diagnosis %in% c("A_IDH","A_IDH_LG","A_IDH_HG") == F, "other", methylation.sub.diagnosis))
+  dplyr::mutate(mnp_predictBrain_v12.8_cal_class = ifelse(mnp_predictBrain_v12.8_cal_class %in% c("A_IDH","A_IDH_LG","A_IDH_HG") == F, "other", mnp_predictBrain_v12.8_cal_class))
 
 
 ggplot(out.probe_based, aes(
-  y=LGC.lasso.probe_based, 
-  x=IDH_HG_IDH_ratio, 
-  col=methylation.sub.diagnosis
+  y=LGC.lasso.probe_based,
+  x=A_IDH_HG__A_IDH_LG_lr,
+  col=mnp_predictBrain_v12.8_cal_class
+  #label=sentrix_id
 )) +
-  geom_point() +
-  labs(subtitle = "Linear probe-based classifier predicting log(IDH_HG / IDH[_LG]) ratio [10x CV]")
-
-
-rm(out.probe_based)
-
-
-
-
-## export ----
+  geom_point(size=1.5) +
+  ggpubr::stat_cor(method = "spearman", aes(label = ..r.label..), col="1") +
+  labs(subtitle = "Linear probe-based classifier predicting log(IDH_HG / IDH[_LG]) ratio [10x CV]")+
+  theme_bw() +
+  theme(legend.position = 'bottom')
 
 
 
 
-lm.full.probe_based <- glmnet::glmnet(data.all |> 
-                            dplyr::select(metadata.all$methylation.sid) |>
-                            t(),
-                          metadata.all$IDH_HG_IDH_ratio,
-                          alpha = 1,
-                          lambda = cv_model_probe_based$lambda.min)
+## build model on full data ----
 
-#saveRDS(rownames(data.all), file="cache/LGC_predictor_probe_based_ordered_probes.Rds")
-#saveRDS(lm.full.probe_based, file="cache/LGC_predictor_probe_based_lm.Rds")
-#lm.full.probe_based <- readRDS("cache/LGC_predictor_probe_based_lm.Rds")
+
+lm.full.probe_based <- glmnet::glmnet(data.glass_nl |> 
+                                      dplyr::select(metadata.glass_nl$sentrix_id) |>
+                                      t(),
+                                    metadata.glass_nl$A_IDH_HG__A_IDH_LG_lr,
+                                    alpha = 1, lambda = cv_model_probe_based$lambda.min)
+
+
+saveRDS(rownames(data.glass_nl), file="cache/LGC_predictor_probe_based_ordered_probes.Rds")
+saveRDS(lm.full.probe_based, file="cache/LGC_predictor_probe_based_lm.Rds")
+
+
+
 
 # probes <- names(lm.full.probe_based$beta[which(lm.full.probe_based$beta != 0),1])
 
 
-# on PCA ----
+# model 2: on PCA ----
+
+## train parameters ----
 
 rm(cv_model.pca_based, lm)
 metadata.test$LGC.lasso.pca_based <- NULL
 
 set.seed(123456)
 cv_model.pca_based <- glmnet::cv.glmnet(
-  data.all.pca |>  dplyr::select(metadata.all$methylation.sid) |> 
+  data.glass_nl.pca |>  dplyr::select(metadata.glass_nl$sentrix_id) |> 
     t(),
-                              metadata.all$IDH_HG_IDH_ratio, alpha = 1, relax=F)
+                              metadata.glass_nl$A_IDH_HG__A_IDH_LG_lr, alpha = 1, relax=F)
 plot(cv_model.pca_based) 
 
 
@@ -137,29 +214,29 @@ rm(out.pca_based)
 out.pca_based <- data.frame()
 for(cv in 0:9) {
   
-  metadata.train <- metadata.all |> dplyr::filter(m != cv)
-  metadata.test <- metadata.all |> dplyr::filter(m == cv)
+  metadata.train <- metadata.glass_nl |> dplyr::filter(slice != cv)
+  metadata.test <- metadata.glass_nl |> dplyr::filter(slice == cv)
   
   set.seed(123456)
   lm.pca_based <- glmnet::glmnet(
-    data.all.pca |>
-      dplyr::select(metadata.train$methylation.sid) |> 
+    data.glass_nl.pca |>
+      dplyr::select(metadata.train$sentrix_id) |> 
       t(),
-              metadata.train$IDH_HG_IDH_ratio, 
+              metadata.train$A_IDH_HG__A_IDH_LG_lr, 
     alpha = 1,
     lambda = cv_model.pca_based$lambda.min)
   
   
   metadata.test$LGC.lasso.pca_based <- predict(lm.pca_based, 
-                                               data.all.pca |> 
-                                                 dplyr::select(metadata.test$methylation.sid) |> t()
+                                               data.glass_nl.pca |> 
+                                                 dplyr::select(metadata.test$sentrix_id) |> t()
                                                ) |> 
     as.numeric()
-  sqrt(sum((metadata.test$LGC.lasso.pca_based - metadata.test$IDH_HG_IDH_ratio)^2) / length(metadata.test$IDH_HG_IDH_ratio))
+  sqrt(sum((metadata.test$LGC.lasso.pca_based - metadata.test$A_IDH_HG__A_IDH_LG_lr)^2) / length(metadata.test$A_IDH_HG__A_IDH_LG_lr))
   
   
   out.pca_based <- rbind(
-    metadata.test |>  dplyr::select(methylation.sid, LGC.lasso.pca_based, IDH_HG_IDH_ratio, methylation.sub.diagnosis)
+    metadata.test |>  dplyr::select(sentrix_id, LGC.lasso.pca_based, A_IDH_HG__A_IDH_LG_lr, mnp_predictBrain_v12.8_cal_class)
     , out.pca_based
   )
 }
@@ -167,16 +244,22 @@ for(cv in 0:9) {
 
 
 out.pca_based <- out.pca_based |> 
-  dplyr::mutate(methylation.sub.diagnosis = ifelse(methylation.sub.diagnosis %in% c("A_IDH","A_IDH_LG","A_IDH_HG") == F, "other", methylation.sub.diagnosis))
+  dplyr::mutate(mnp_predictBrain_v12.8_cal_class = ifelse(mnp_predictBrain_v12.8_cal_class %in% c("A_IDH","A_IDH_LG","A_IDH_HG") == F, "other", mnp_predictBrain_v12.8_cal_class))
 
 
 ggplot(out.pca_based, aes(
-  y=LGC.lasso.pca_based, 
-  x=IDH_HG_IDH_ratio, 
-  col=methylation.sub.diagnosis
-)) +
-  geom_point() +
-  labs(subtitle = "Linear PCA-based classifier predicting log(IDH_HG / IDH[_LG]) ratio [10x CV]")
+    y=LGC.lasso.pca_based,
+    x=A_IDH_HG__A_IDH_LG_lr,
+    col=mnp_predictBrain_v12.8_cal_class
+    #label=sentrix_id
+  )) +
+    geom_point(size=1.5) +
+    ggpubr::stat_cor(method = "spearman", aes(label = ..r.label..), col="1") +
+    labs(subtitle = "Linear PCA-based classifier predicting log(IDH_HG / IDH[_LG]) ratio [10x CV]")+
+    theme_bw() +
+    theme(legend.position = 'bottom')
+  
+  
 
 
 
@@ -184,31 +267,34 @@ ggplot(out.pca_based, aes(
 
 
 plt <- out.probe_based |> 
-  dplyr::left_join(out.pca_based |> dplyr::select(methylation.sid, `LGC.lasso.pca_based`),
-                   by=c('methylation.sid'='methylation.sid'))
+  dplyr::left_join(out.pca_based |> dplyr::select(sentrix_id, `LGC.lasso.pca_based`),
+                   by=c('sentrix_id'='sentrix_id'))
 
 
 ggplot(plt, aes(x=LGC.lasso.probe_based, y=LGC.lasso.pca_based )) +
   geom_point()
 
 
-## export to reproduce ----
+## build model on full data ----
 
 
-lm.full.pca_based <- glmnet::glmnet(data.all.pca |> 
-                            dplyr::select(metadata.all$methylation.sid) |>
+lm.full.pca_based <- glmnet::glmnet(data.glass_nl.pca |> 
+                            dplyr::select(metadata.glass_nl$sentrix_id) |>
                             t(),
-                     metadata.all$IDH_HG_IDH_ratio,
-                     alpha = 1, lambda = cv_model$lambda.min)
+                     metadata.glass_nl$A_IDH_HG__A_IDH_LG_lr,
+                     alpha = 1, lambda = cv_model.pca_based$lambda.min)
 
-saveRDS(data.all.pca.obj, file="cache/LGC_predictor_PCA_based_prcomp.Rds")
+saveRDS(data.glass_nl.pca.obj, file="cache/LGC_predictor_PCA_based_prcomp.Rds")
 saveRDS(lm.full.pca_based, file="cache/LGC_predictor_PCA_based_lm.Rds")
+
+
 
 
 # apply to OD samples ----
 
+
 classifier <- readRDS("cache/LGC_predictor_probe_based_lm.Rds")
-data <- glass_od.data.mvalues |> 
+data <- data.glass_od |> 
   tibble::rownames_to_column('probeID') |> 
   dplyr::filter(probeID %in% rownames(classifier$beta)) |> 
   tibble::column_to_rownames('probeID') |> 
@@ -221,10 +307,24 @@ data <- glass_od.data.mvalues |>
 stopifnot(rownames(classifier$beta) == colnames(data))
 stopifnot(readRDS("cache/LGC_predictor_probe_based_ordered_probes.Rds") == colnames(data))
 
-p = predict(classifier, data) |> 
+
+p <- predict(classifier, data) |> 
   as.data.frame() |> 
-  dplyr::rename(`A_IDH_A_IDH_HG_lasso_fit` = 1) |> 
+  dplyr::rename(`A_IDH_HG__A_IDH_LG_lr__lasso_fit` = 1) |> 
   tibble::rownames_to_column('sentrix_id')
+
+
+## export ----
+
+
+saveRDS(p, file="cache/GLASS-OD__A_IDH_HG__A_IDH_LG_lr__lasso_fit.Rds")
+
+
+
+
+## sandbox ----
+
+
 
 plt <- glass_od.metadata.idats |> 
   dplyr::left_join(p, by=c('sentrix_id'='sentrix_id'),suffix=c('','')) |> 
@@ -234,32 +334,32 @@ plt <- glass_od.metadata.idats |>
 
 
 
-ggplot(plt, aes(x=A_IDH_A_IDH_HG_lasso_fit, y=resection_tumor_grade)) + 
+ggplot(plt, aes(x=A_IDH_HG__A_IDH_LG_lr__lasso_fit, y=resection_tumor_grade)) + 
   geom_point()
 
 
-#ggplot(plt, aes(x=A_IDH_A_IDH_HG_lasso_fit, y=PC3, col=as.factor(predictBrain_12.5_cal_class), label=label)) + 
-ggplot(plt, aes(x=A_IDH_A_IDH_HG_lasso_fit, y=PC3, col=qc.pca.outlier, label=label)) + 
+#ggplot(plt, aes(x=A_IDH_HG__A_IDH_LG_lr__lasso_fit, y=PC3, col=as.factor(predictBrain_12.5_cal_class), label=label)) + 
+ggplot(plt, aes(x=A_IDH_HG__A_IDH_LG_lr__lasso_fit, y=PC3, col=qc.pca.outlier, label=label)) + 
   geom_point() +
   labs(y = "Generic PCA / PC3 in all Oligo samples")
   #ggrepel::geom_text_repel(col="blue", size=2.5 , segment.size=0.35, show.legend  = FALSE) 
 
 
-ggplot(plt |>  dplyr::filter(qc.pca.outlier == F), aes(x=A_IDH_A_IDH_HG_lasso_fit, y=PC3, col=as.factor(predictBrain_12.5_cal_class), label=label)) + 
+ggplot(plt |>  dplyr::filter(qc.pca.outlier == F), aes(x=A_IDH_HG__A_IDH_LG_lr__lasso_fit, y=PC3, col=as.factor(predictBrain_12.5_cal_class), label=label)) + 
   ggpubr::stat_cor(method = "spearman", aes(label = ..r.label..), col="1") +
   geom_point() +
   labs(y = "Generic PCA / PC3 in all Oligo samples") + 
   theme_bw()
 
 
-ggplot(plt |>  dplyr::filter(qc.pca.outlier == F), aes(x=A_IDH_A_IDH_HG_lasso_fit, y=PC3, col=as.factor(resection_tumor_grade))) + 
+ggplot(plt |>  dplyr::filter(qc.pca.outlier == F), aes(x=A_IDH_HG__A_IDH_LG_lr__lasso_fit, y=PC3, col=as.factor(resection_tumor_grade))) + 
   ggpubr::stat_cor(method = "spearman", aes(label = ..r.label..), col="1") +
   geom_point() +
   labs(y = "Generic PCA / PC3 in all Oligo samples") + 
   theme_bw()
 
 
-ggplot(plt, aes(x=A_IDH_A_IDH_HG_lasso_fit, y=PC4)) + 
+ggplot(plt, aes(x=A_IDH_HG__A_IDH_LG_lr__lasso_fit, y=PC4)) + 
   geom_point()
 
 
