1cfcb48214480037aaa297f01255973756d3bfea
yhoogstrate <y.hoogstrate@gmail.com>
2023-08-14 15:57:10 +0200

    update filter

--- scripts/func_filter_first_G2_and_last_G3.R
+++ scripts/func_filter_first_G2_and_last_G3.R
@@ -2,18 +2,36 @@
 
 
 filter_first_G2_and_last_G3 <- function(metadata, nrow.check = 0) {
-  out <- metadata |> 
+  out <- metadata
+  
+  if('resection_tumor_grade' %in% colnames(metadata)) {
+    out <- out |> 
+      dplyr::mutate(LG_HG_status = dplyr::case_when(
+        resection_tumor_grade %in% c(2) ~ "LG",
+        resection_tumor_grade %in% c(3) ~ "HG",
+        T ~ as.character(NA),
+        ))
+  } else if('WHO_Classification2021' %in% colnames(metadata)) {
+    out <- out |> 
+      dplyr::mutate(LG_HG_status = dplyr::case_when(
+        WHO_Classification2021 %in% c("Astrocytoma, IDH-mutant, WHO grade 2", "Astrocytoma, IDH-mutant, WHO grade 3") ~ "LG",
+        WHO_Classification2021 %in% c("Astrocytoma, IDH-mutant, WHO grade 4") ~ "HG",
+        T ~ as.character(NA),
+      ))
+  }
+  
+  out <- out |> 
     dplyr::group_by(patient_id) |> 
-    dplyr::mutate(first_G2 = 
-                    !is.na(resection_tumor_grade) & 
-                    resection_tumor_grade == 2 &
+    dplyr::mutate(first_LG = 
+                    !is.na(LG_HG_status) & 
+                    LG_HG_status == "LG" &
                     resection_number == min(resection_number)) |>
-    dplyr::mutate(last_G3 = 
-                    !is.na(resection_tumor_grade) & 
-                    resection_tumor_grade == 3 &
+    dplyr::mutate(last_HG = 
+                    !is.na(LG_HG_status) & 
+                    LG_HG_status == "HG" &
                     resection_number == max(resection_number)) |>
     dplyr::ungroup() |>
-    dplyr::filter(first_G2 | last_G3)
+    dplyr::filter(first_LG | last_HG)
   
   
   if(nrow.check > 0) {
@@ -24,7 +42,6 @@ filter_first_G2_and_last_G3 <- function(metadata, nrow.check = 0) {
         return(.)
       })()
   }
-  
   
   return(out)
 }
