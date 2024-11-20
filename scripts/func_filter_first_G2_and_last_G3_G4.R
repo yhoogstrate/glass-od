a75db5780aada397f53f4ac280c7f8f6d39c9c89
yhoogstrate <y.hoogstrate@gmail.com>
2024-11-20 12:51:38 +0100

    sav

--- scripts/func_filter_first_G2_and_last_G3_G4.R
+++ scripts/func_filter_first_G2_and_last_G3_G4.R
@@ -1,15 +1,15 @@
 #!/usr/bin/env R
 
 
-filter_first_G2_and_last_G3 <- function(metadata, nrow.check = 0) {
-  stopifnot('resection_tumor_grade' %in% colnames(metadata)) # only Oligo's
+filter_first_G2_and_last_G3_G4 <- function(metadata, nrow.check = 0) {
+  stopifnot('WHO_Classification2021' %in% colnames(metadata))
 
   out <- metadata |> 
     dplyr::mutate(LG_HG_status = dplyr::case_when(
-      resection_tumor_grade %in% c(2) ~ "LG",
-      resection_tumor_grade %in% c(3) ~ "HG",
+      WHO_Classification2021 %in% c("Astrocytoma, IDH-mutant, WHO grade 2") ~ "LG",
+      WHO_Classification2021 %in% c("Astrocytoma, IDH-mutant, WHO grade 3", "Astrocytoma, IDH-mutant, WHO grade 4") ~ "HG",
       T ~ as.character(NA),
-      ))
+    ))
 
   out <- out |> 
     dplyr::group_by(patient_id, LG_HG_status) |> 
