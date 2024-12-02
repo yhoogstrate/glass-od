542361c74f4cc4a54f87110347928b3be0f721a7
yhoogstrate <y.hoogstrate@gmail.com>
2024-12-02 12:12:02 +0100

    sav

--- scripts/load_mvalues_hq_samples.R
+++ scripts/load_mvalues_hq_samples.R
@@ -406,6 +406,35 @@ if(file.exists(fn)) {
 rm(fn)
 
 
+### Oligosarcoma A_IDH_HG ----
+
+
+
+fn <- "cache/analysis_differential__Oligosarcoma_A_IDH_HG__PC1__stats.Rds"
+if(file.exists(fn)) {
+  
+  tmp <- readRDS(fn) |> 
+    #dplyr::select(probe_id, t, adj.P.Val) |> 
+    dplyr::rename_with(~paste0("DMP__GLASS_OD__oligosarcoma__A_IDH_HG__PC1__", .x), .cols=!matches("^probe_id$",perl = T)) |> 
+    (function(.) {
+      print(dim(.))
+      assertthat::assert_that(nrow(.) == CONST_N_PROBES_UNMASKED_AND_DETP)
+      return(.)
+    })()
+  
+  
+  data.mvalues.probes <- data.mvalues.probes |> 
+    dplyr::left_join(tmp, by=c('probe_id'='probe_id'), suffix=c('','') )
+  
+  rm(tmp)
+  
+} else {
+  warning("DMP result primary - recurrence is missing")
+}
+
+rm(fn)
+
+
 
 ### GLASS-NL ----
 #### prim rec naive GLASS-NL ----
