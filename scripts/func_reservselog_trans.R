c3ee493200e6a1208b19e14966bc6f376957b850
yhoogstrate <y.hoogstrate@gmail.com>
2024-12-03 16:13:23 +0100

    patch

--- scripts/func_reservselog_trans.R
+++ scripts/func_reservselog_trans.R
@@ -1,11 +1,14 @@
 #!/usr/bin/env R
 
+# https://stackoverflow.com/questions/11053899/how-to-get-a-reversed-log10-scale-in-ggplot2
+
 #library(scales)
 
 reverselog_trans <- function(base = exp(1)) {
   trans <- function(x) -log(x, base)
   inv <- function(x) base^(-x)
-  trans_new(paste0("reverselog-", format(base)), trans, inv, 
+  scales::trans_new(paste0("reverselog-", format(base)), trans, inv, 
             scales::log_breaks(base = base), 
             domain = c(1e-100, Inf))
 }
+
