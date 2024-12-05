3d14c89bfb79ec008b30e53f7a7982bd4d5b707c
yhoogstrate <y.hoogstrate@gmail.com>
2024-12-05 16:21:20 +0100

    sav

--- scripts/vis_KI67_classifications.R
+++ scripts/vis_KI67_classifications.R
@@ -18,30 +18,41 @@ if(!exists('glass_od.metadata.array_samples')) {
 ## figs ----
 
 
-
-
-ggplot(glass_od.metadata.array_samples, aes(x=staining_KI67_lr_pos_neg_cells, y=array_PC3)) +
-  geom_point()
-
-ggplot(glass_od.metadata.array_samples, aes(x=log(staining_KI67_pos_per_area_um2), y=array_PC3)) +
-  geom_point()
-
-
-
-
-ggplot(glass_od.metadata.array_samples, aes(x=staining_KI67_lr_pos_neg_cells, y=array_PC2)) +
-  geom_point()
-
-ggplot(glass_od.metadata.array_samples, aes(x=log(staining_KI67_pos_per_area_um2), y=array_PC2)) +
-  geom_point()
+plt <- glass_od.metadata.array_samples |> 
+  dplyr::filter(!is.na(staining_KI67_pos_per_area_um2)) |> 
+  dplyr::mutate(col = ifelse(
+    array_mnp_predictBrain_v12.8_cal_class %in% c("GBM_MES_TYP", "GBM_RTK1"), "Other", array_mnp_predictBrain_v12.8_cal_class
+  )) |> 
+  dplyr::mutate(highlighted = staining_KI67_filename %in% c("KI67_09-5718 I.ndpi","KI67_20-9179 B.ndpi","KI67_22-3789 I-C.ndpi","KI67_H04-10641 I.ndpi"))
 
 
 
 
+ggplot(plt, aes(x=log(staining_KI67_pos_per_area_um2), y=array_PC2, col=col)) +
+  geom_point(size=theme_nature_size/3) +
+  scale_color_manual(values = c(palette_mnp_12.8_6, `Other`="darkgray")) +
+  theme_nature
 
-ggplot(glass_od.metadata.array_samples, aes(x=log(staining_KI67_pos_per_area_um2), 
-                                            y=array_dnaMethyAge__PCHorvathS2018)) +
-  geom_point()
+
+ggplot(plt, aes(x=log(staining_KI67_pos_per_area_um2), y=array_PC3, col=col)) +
+  geom_point(size=theme_nature_size/3) +
+  scale_color_manual(values = c(palette_mnp_12.8_6, `Other`="darkgray")) +
+  theme_nature
+
+
+ggplot(plt, aes(x=log(staining_KI67_pos_per_area_um2), 
+                                            y=-1 * array_dnaMethyAge__PCHorvathS2018,
+                , col=col)) +
+  geom_point(size=theme_nature_size/3) + 
+  scale_color_manual(values = c(palette_mnp_12.8_6, `Other`="darkgray")) +
+  theme_nature
+
+
+
+
+
+
+
 
 
 
@@ -49,6 +60,14 @@ ggplot(glass_od.metadata.array_samples, aes(x=log(staining_KI67_pos_per_area_um2
 #                                             y=array_dnaMethyAge__PCHorvathS2018)) +
 #   geom_point()
 
+
+
+# ggplot(glass_od.metadata.array_samples, aes(x=staining_KI67_lr_pos_neg_cells, y=array_PC2)) +
+#   geom_point()
+
+
+# ggplot(glass_od.metadata.array_samples, aes(x=staining_KI67_lr_pos_neg_cells, y=array_PC3)) +
+#   geom_point()
 
 
 
@@ -132,22 +151,41 @@ plt <- glass_od.metadata.array_samples |>
   dplyr::filter(!is.na(staining_KI67_lr_pos_neg_cells)) |> 
   dplyr::mutate(resection_grade = paste0("WHO Grade ", resection_tumor_grade)) |> 
   dplyr::mutate(resection_type = ifelse(resection_number == 1, "primary", "recurrent")) |> 
-  dplyr::mutate(rank = order(order(staining_KI67_pos_per_detected_cells)))
+  dplyr::mutate(rank = order(order(staining_KI67_pos_per_detected_cells))) |> 
+  dplyr::mutate(col = ifelse(
+    array_mnp_predictBrain_v12.8_cal_class %in% c("GBM_MES_TYP", "GBM_RTK1"), "Other", array_mnp_predictBrain_v12.8_cal_class
+  )) |> 
+  dplyr::filter(!is.na(staining_KI67_pos_per_detected_cells)) |> 
+  dplyr::mutate(highlighted = staining_KI67_filename %in% c("KI67_09-5718 I.ndpi","KI67_20-9179 B.ndpi","KI67_22-3789 I-C.ndpi","KI67_H04-10641 I.ndpi"))
 
 
-plt |> dplyr::select(resection_id, resection_number, staining_KI67_filename, staining_KI67_pos_per_detected_cells) |> 
-  dplyr::mutate(pct = staining_KI67_pos_per_detected_cells * 100) |> 
-  View()
+# plt |> dplyr::select(resection_id, resection_number, staining_KI67_filename, staining_KI67_pos_per_detected_cells) |> 
+#   dplyr::mutate(pct = staining_KI67_pos_per_detected_cells * 100) |> 
+#   View()
 
 
 
-
-ggplot(plt, aes(x=reorder(resection_id, rank), y=staining_KI67_pos_per_detected_cells)) +
-  geom_point() +
-  scale_y_continuous(labels = scales::percent) +
+ggplot(plt, aes(x=reorder(resection_id, rank),
+                y=staining_KI67_pos_per_detected_cells,
+                col=col,
+                label=paste0(round(staining_KI67_pos_per_detected_cells * 100,1),"%"))) +
   geom_hline(yintercept=0.05, col="red", lty=2,lwd=theme_nature_lwd) +
   geom_hline(yintercept=0.1, col="red", lty=2,lwd=theme_nature_lwd) +
-  theme_nature
+  scale_y_continuous(labels = scales::percent) +
+  geom_point(size=theme_nature_size/3) +
+  ggrepel::geom_text_repel(data=subset(plt, highlighted),
+                           col="black",
+                           size=theme_nature_size,
+                           family=theme_nature_font_family,
+                           segment.size=theme_nature_lwd,
+                           nudge_y = 0.075) +
+  scale_color_manual(values = c(palette_mnp_12.8_6, `Other`="darkgray")) +
+  theme_nature +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(x='resection', y="Percentage cells classified Ki67+", col=NULL)
+
+ggsave("output/figures/vis_KI67_classifications.pdf", width=0.975 * 8.5, height=2.25)
+
 
 
 
